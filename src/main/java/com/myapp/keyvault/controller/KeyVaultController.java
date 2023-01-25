cf0a9834d5a6abdcd15584de6c5914a9317bf616
devil730 <a30sham.07@gmail.com>
2023-01-25 15:55:21 +0530

    List down key vault secret using springboot application

--- src/main/java/com/myapp/keyvault/controller/KeyVaultController.java
+++ src/main/java/com/myapp/keyvault/controller/KeyVaultController.java
@@ -7,12 +7,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class KeyVaultController {
 
-	@Value("${connectionString}")
+	@Value("${spring.datasource.url}")
 	private String connectionString;
+
+	@Value("${spring.datasource.username}")
+	private String dbuser;
+
+	@Value("${spring.datasource.password}")
+	private String dbpass;
 
 	@GetMapping("/getsecret")
 	public String get() {
-		return "Your Stored Secret Key is " + connectionString;
+		System.out.println("\nYour Stored Secret Key is " + connectionString + " " + dbuser + " " + dbpass);
+		return "Your Stored Secret Key is " + connectionString + " " + dbuser + " " + dbpass;
 	}
 
 }
