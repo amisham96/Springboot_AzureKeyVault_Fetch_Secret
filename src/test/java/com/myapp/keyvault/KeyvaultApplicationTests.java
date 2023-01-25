735ef98f82d8f722be27ce8536cbd4541e19af06
devil730 <a30sham.07@gmail.com>
2023-01-25 12:00:12 +0530

    List down secret from Azure Keyvault using springboot

---
+++ src/test/java/com/myapp/keyvault/KeyvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.keyvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
