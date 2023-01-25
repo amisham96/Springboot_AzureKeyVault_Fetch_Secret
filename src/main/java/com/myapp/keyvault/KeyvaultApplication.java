da356c2f4b8c88dc87ca15b25ad55fb8c624f04a
devil730 <a30sham.07@gmail.com>
2023-01-25 12:22:51 +0530

    List down secret from Azure Keyvault using springboot

--- src/main/java/com/myapp/keyvault/KeyvaultApplication.java
+++ src/main/java/com/myapp/keyvault/KeyvaultApplication.java
@@ -1,32 +1,13 @@
 package com.myapp.keyvault;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-public class KeyvaultApplication implements CommandLineRunner {
-
-	@Value("${connectionString}")
-	private String connectionString;
+public class KeyvaultApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KeyvaultApplication.class, args);
-	}
-
-	@GetMapping("get")
-	public String get() {
-		return connectionString;
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		System.out.println("\nSecret stored in Azure Key Vault: \n" + connectionString);
 	}
 
 }
---
+++ src/main/java/com/myapp/keyvault/controller/KeyVaultController.java
@@ -0,0 +1,18 @@
+package com.myapp.keyvault.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KeyVaultController {
+
+	@Value("${connectionString}")
+	private String connectionString;
+
+	@GetMapping("/getsecret")
+	public String get() {
+		return "Your Stored Secret is " + connectionString;
+	}
+
+}
